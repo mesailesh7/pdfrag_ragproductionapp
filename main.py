209b8620ce4ed1a24e0a5e83f0440141b0a9f841
Sonu Thapa <37073011+mesailesh7@users.noreply.github.com>
2026-03-30 01:03:03 -0600

    Initial commit

---
+++ main.py
@@ -0,0 +1,33 @@
+import datetime
+import logging
+import os
+import uuid
+
+import inngest
+import inngest.fast_api
+from dotenv import load_dotenv
+from fastapi import FastAPI
+from inngest.experimental import ai
+from inngest.experimental.mocked import trigger
+
+load_dotenv()
+
+inngest_client = inngest.Inngest(
+    app_id="rag_app",
+    logger=logging.getLogger("uvicorn"),
+    is_production=False,
+    serializer=inngest.PydanticSerializer(),
+)
+
+
+@inngest_client.create_function(
+    fn_id="RAG: Inngest PDf", trigger=inngest.TriggerEvent(event="rag/ingest_pdf")
+)
+async def rag_inngest_pdf(ctx: inngest.Context):
+    return {"Hello": "world"}
+
+
+app = FastAPI()
+
+
+inngest.fast_api.serve(app, inngest_client, [])
